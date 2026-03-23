0503c3d9a0bfdf654cd85351f130184286bca22f
ritesh3397 <ritesh.12mzp@gmail.com>
2026-03-23 07:29:03 +0530

    Add configuration loading from environment variables

---
+++ Config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GROQ_API_KEY  = os.getenv("GROQ_API_KEY")
+SUPABASE_URL  = os.getenv("SUPABASE_URL")
+SUPABASE_KEY  = os.getenv("SUPABASE_KEY")
+SECRET_KEY    = os.getenv("SECRET_KEY", "change-me")
